f9fe595c97b1e1e32547635045d4b58796d2f9fc
galalkoro <koro-galal@outlook.com>
2020-06-17 20:26:38 +0200

    first I did not understand and just creat a new files to solve

---
+++ exercises/01-variables/exercises/2-let1.js
@@ -0,0 +1,17 @@
+// practice using variables declared with let
+//  fill in the blanks to log the commented values
+// and don't just write the correct answer directly!
+//  practice re-using values stored in variables to get the correct value
+
+let fruit = 'banana';
+let desert = 'cake';
+let topping = 'frosting';
+
+desert = `chocolate ${desert}`;
+console.log(desert); // should log 'chocolate cake'
+
+desert = `${desert} with ${topping}`;
+console.log(desert); // should log 'chocolate cake with frosting'
+
+desert = `${desert} and ${fruit}`;
+console.log(desert); // should log 'chocolate cake with frosting and banana'
