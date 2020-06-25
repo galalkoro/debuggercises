f06e3d0c4ce6e79fbc0d7db7bb703ae821212cbb
galalkoro <koro-galal@outlook.com>
2020-06-25 15:57:31 +0200

    exercise 10 statements finished

--- exercises/10-conditional-statements/1-execution-paths/4-write-function.js
+++ exercises/10-conditional-statements/1-execution-paths/4-write-function.js
@@ -1,15 +1,15 @@
 const mystery = (a, b) => {
-  let result = '';
-  if (_) {
-    result = 'path 1';
-  } else if (_) {
-    result = 'path 2';
-  } else if (_) {
-    result = 'path 3';
-  } else {
-    result = 'path 4';
-  }
-  return result;
+	let result = '';
+	if (a && b) {
+		result = 'path 1';
+	} else if (!a && !b) {
+		result = 'path 2';
+	} else if (typeof a === typeof b) {
+		result = 'path 3';
+	} else {
+		result = 'path 4';
+	}
+	return result;
 };
 
 // path 1
